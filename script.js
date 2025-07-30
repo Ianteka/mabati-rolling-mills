814b3b78b1e89e82ed769825e4f6060f2305d865
ianteka <ianteka01@gmail.com>
2025-07-30 16:04:59 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+function toggleSection(id) {
+  const list = document.getElementById(id);
+  list.style.display = (list.style.display === 'none' || list.style.display === '') ? 'block' : 'none';
+}
+const sliderImages = [
+  "Assets/constructedhouse.jpeg",
+  "Assets/Usualsheet.jpeg",
+  "Assets/Cement.jpeg"
+];
